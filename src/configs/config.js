a6fdd6577b1774ae3ea000a900c0ac46e51f4aeb
ManzXD <aimanmk89@gmail.com>
2025-02-24 19:48:06 +0800

    Update config.js

--- src/configs/config.js
+++ src/configs/config.js
@@ -2,8 +2,8 @@ import { config } from "dotenv";
 config();
 
 export default {
-  owner: ["6285691464024"], // isi
-  pairingNumber: "6285691464024", // isi 
+  owner: ["60102810046"], // isi
+  pairingNumber: "60102810046", // isi 
   self: true, 
   autoRead: {
     story: true,
